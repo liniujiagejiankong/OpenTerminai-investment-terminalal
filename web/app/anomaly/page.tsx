1bcb0977636b255c1361a602c825994f1d3f9ef4
liniujiagejiankong <jiagejiankong01@163.com>
2026-09-09 12:27:24 +0800

    Create page.tsx

---
+++ web/app/anomaly/page.tsx
@@ -0,0 +1,11 @@
+'client';
+import React from 'react';
+import AnomalyRadar from '../../src/components/AnomalyRadar';
+
+export default function AnomalyPage() {
+  return (
+    <main>
+      <AnomalyRadar />
+    </main>
+  );
+}
