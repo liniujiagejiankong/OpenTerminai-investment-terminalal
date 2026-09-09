7275f50549e9c896c56826367762300bafb88b02
liniujiagejiankong <jiagejiankong01@163.com>
2026-09-09 14:33:15 +0800

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').Next.jsConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/OpenTerminal-investment-terminalal', // 必须加上你的仓库名
+  basePath: '/OpenTerminal-investment-terminalal',
   assetPrefix: '/OpenTerminal-investment-terminalal/',
   eslint: {
     ignoreDuringBuilds: true,
